c296c716cea01e536f9be1848160fff68398c750
enes <enuxuna@gmail.com>
2018-11-27 16:49:18 +0300

    added search component and installed react-router

--- src/App/page/App.js
+++ src/App/page/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
+
+import Search from "../components/Search/Search";
+
 import './App.css'
+
 import { getJSON } from "../../request"
 
 export default class App extends Component {
-  componentDidMount() {
-    getJSON({ url: "characters" })
-      .then((characters) => {
-        console.log(characters)
-      })
-      .catch((error) => {
-        console.error(error)
-      })
-  }
+  // componentDidMount() {
+  //   getJSON({ url: "characters" })
+  //     .then((characters) => {
+  //       console.log(characters)
+  //     })
+  //     .catch((error) => {
+  //       console.error(error)
+  //     })
+  // }
 
   render() {
     return (
-      <div />
+      <Search />
     );
   }
 }
