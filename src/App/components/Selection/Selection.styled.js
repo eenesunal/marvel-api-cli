5b780cbafea36da6e476a05f35af8f94c0da3f25
eenesunal <enuxuna@gmail.com>
2018-12-17 20:14:14 +0300

    removed hover animation

--- src/App/components/Selection/Selection.styled.js
+++ src/App/components/Selection/Selection.styled.js
@@ -61,38 +61,6 @@ export const Overlay = styled.div`
     left: 0;
 
     background-color: rgba(0, 0, 0, .6);
-    box-shadow: 0 0 0 100px inset, 0 0 5px grey;
-    transition: box-shadow 1s;
-
-    &::after {
-        width: 80%;
-        height: 80%;
-        display: block;
-        font:  16pt 'bookman old syle' ;
-        color: white;
-        border: 2px solid;
-        text-align: center;
-        
-        /* center content using flex box */
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        
-        /* hover out transition */
-        transition: opacity 1s .5s;
-    }
- 
-    /* reveal pokemon picture on hover */
-    &:hover {
-        transition: box-shadow 1s;
-        box-shadow: 0 0 0 5px inset, 0 0 5px grey, 0 0 10px grey inset;
-    }
-    
-    /* hide pokemon name on hover */
-    &:hover::after {
-        transition: opacity .5s;
-        opacity: 0;
-    }
 `;
 
 export const Title = styled.span`
