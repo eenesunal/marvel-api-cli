0e1178a06a860ff53026cf72f691cffe2b2ed99b
enes <enuxuna@gmail.com>
2019-04-04 11:17:44 +0300

    Updates

---
+++ precache-manifest.c3b2656822946f6dac4edf54cd2a82c1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "560573aa10cc7ed149e627569eb40a1b",
+    "url": "/marvel-api-cli/static/media/search-background.560573aa.jpg"
+  },
+  {
+    "revision": "229eadb97d1d735a945a",
+    "url": "/marvel-api-cli/static/css/main.72e46760.chunk.css"
+  },
+  {
+    "revision": "d076becced3b3a16d5f1",
+    "url": "/marvel-api-cli/static/js/1.d076becc.chunk.js"
+  },
+  {
+    "revision": "4f6a25e116bc0217bc45",
+    "url": "/marvel-api-cli/static/js/runtime~main.4f6a25e1.js"
+  },
+  {
+    "revision": "03e4eae602b7c7d753b59441fca4ba09",
+    "url": "/marvel-api-cli/static/media/logo.03e4eae6.png"
+  },
+  {
+    "revision": "229eadb97d1d735a945a",
+    "url": "/marvel-api-cli/static/js/main.229eadb9.chunk.js"
+  },
+  {
+    "revision": "ad1cc3934285de3ee4e58183e960de4e",
+    "url": "/marvel-api-cli/static/media/Marvel-Regular.ad1cc393.ttf"
+  },
+  {
+    "revision": "01bd3ccacea9b68eb76f9c9fe898437a",
+    "url": "/marvel-api-cli/static/media/Marvel-Bold.01bd3cca.ttf"
+  },
+  {
+    "revision": "581d548316d8e4d66c05f91800d4fde9",
+    "url": "/marvel-api-cli/static/media/Marvel-Italic.581d5483.ttf"
+  },
+  {
+    "revision": "b6146edd746acdbd665fbddc922e1eac",
+    "url": "/marvel-api-cli/static/media/Marvel-BoldItalic.b6146edd.ttf"
+  },
+  {
+    "revision": "1c03a713bc6e98b911c6a6885ff9f7ec",
+    "url": "/marvel-api-cli/index.html"
+  }
+];
